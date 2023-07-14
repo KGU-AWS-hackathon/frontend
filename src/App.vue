cb6dff3dc7f718e95b38ce5f3fe74c5554190b34
0521kimyi <0521kimyi@gmail.com>
2023-07-15 02:51:24 +0900

    넷바 수정

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/bike">자전거</router-link> |
-    <router-link to="/kick">킥보드</router-link> |
-    <router-link to="/test">Test</router-link>
+    <router-link to="/kick">킥보드</router-link>
+    <!-- <router-link to="/test">Test</router-link> -->
   </nav>
   <router-view/>
 </template>
@@ -16,17 +16,22 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
-  padding: 30px;
+  padding: 20px;
+  margin-bottom: 10px;
+  background-color: #f2f2f2;
 }
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 5px 10px;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+  background-color: #e6f4ea;
 }
+
 </style>
