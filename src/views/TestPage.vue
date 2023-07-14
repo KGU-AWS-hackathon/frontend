3fcf30dad7769aa35a293c96e882a57452b9e655
0521kimyi <0521kimyi@gmail.com>
2023-07-14 17:21:46 +0900

    axios 테스트

---
+++ src/views/TestPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!-- 데이터를 표시할 요소 -->
+        <p>{{ fetchedData }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            fetchedData: null
+        };
+    },
+    created() {
+        this.getData();
+    },
+    methods: {
+        getData() {
+            axios.get('http://phopho.shop/')
+                .then(response => {
+                    this.fetchedData = response.data.message;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }
+};
+</script>
