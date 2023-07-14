93245d748020ce90d0c899b0c38c77f79e3e5c13
Jhong <ghddl23@naver.com>
2023-07-14 22:45:48 +0900

    위치 변경

--- src/components/MapComp.vue
+++ src/components/MapComp.vue
@@ -1,12 +1,45 @@
 <template>
-    <MapKakao :latitude="37.39843974939604" :longitude="127.10972941510465" />
+    <div>
+        <input v-model="latitudeInput" placeholder="Latitude" />
+        <input v-model="longitudeInput" placeholder="Longitude" />
+        <button v-on:click="updateCoordinates">Update</button>
+        <MapKakao :latitude="latitude" :longitude="longitude" :key="mapKey" />
+    </div>
 </template>
 
 <script>
 import MapKakao from '@/components/Mapkakao.vue';
+
 export default {
     components: {
         MapKakao
+    },
+    data() {
+        return {
+            latitudeInput: '',
+            longitudeInput: '',
+            latitude: 37.55564880, // Default value
+            longitude: 126.91062927, // Default value
+            mapKey: 0 // Key for forcing map reload
+        };
+    },
+    methods: {
+        updateCoordinates() {
+            this.latitude = parseFloat(this.latitudeInput);
+            this.longitude = parseFloat(this.longitudeInput);
+            this.mapKey += 1; // Increment key to force map reload
+        },
+        reloadMap() {
+            this.mapKey += 1; // Increment key to force map reload
+        }
+    },
+    watch: {
+        latitude() {
+            this.reloadMap();
+        },
+        longitude() {
+            this.reloadMap();
+        }
     }
 }
 </script>
